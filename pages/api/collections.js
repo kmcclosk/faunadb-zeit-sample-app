c1bc35e923a3059e8d73fcfddd0fd2781df98eba
Ken McCloskey <ken_mccloskey@discovery.com>
2020-04-26 19:19:50 -0400

    WIP

--- pages/api/collections.js
+++ pages/api/collections.js
@@ -5,26 +5,26 @@ const { FAUNADB_SECRET: secret } = process.env;
 let client;
 
 if (secret) {
-  client = new faunadb.Client({ secret });
+    client = new faunadb.Client({ secret });
 }
 
 export default async (req, res) => {
-  try {
-    let collections = [];
+    try {
+        let collections = [];
 
-    if (!client) {
-      return [];
+        if (!client) {
+            return [];
+        }
+
+        await client
+            .paginate(q.Collections())
+            .map(ref => q.Get(ref))
+            .each(page => {
+                collections = collections.concat(page);
+            });
+
+        res.json({ collections });
+    } catch (error) {
+        res.status(500).json({ error });
     }
-
-    await client
-      .paginate(q.Collections())
-      .map(ref => q.Get(ref))
-      .each(page => {
-        collections = collections.concat(page);
-      });
-
-    res.json({ collections });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
 };
